4e42cf298e3b4b3ad864afeae68e15f419ac2c19
JameMontimar <JamesberthMontimar@gmail.com>
2024-10-03 13:41:06 +0800

    initial commit

---
+++ l01app.js
@@ -0,0 +1,14 @@
+//Input
+const person1 ={
+    name:"James Berth Montimar",
+    age:19,
+    occupation:"student"
+    
+}
+
+//Process and Output
+console.log("Name: "+person1.name);
+console.log("Age: "+person1.age);
+console.log("Occupation: "+person1.occupation);
+
+
