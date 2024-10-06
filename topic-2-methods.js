1464e04774fe091f7b66e2ac6e7cf6886bf18b28
JameMontimar <jamesberthmontimar@gmail.com>
2024-10-06 10:46:17 +0800

    initial commit

---
+++ topic-2-methods.js
@@ -0,0 +1,45 @@
+//Input
+const person1 ={
+    name:"James Berth Montimar",
+    age:19,
+    occupation:"student"
+}
+
+//Process and Output
+console.log("Name: "+person1.name);
+console.log("Age: "+person1.age);
+console.log("Occupation: "+person1.occupation);
+console.log("--------------------------------");
+
+
+// Class definition
+class Person {
+    constructor(name, age, occupation) {
+        this.name = name;
+        this.age = age;
+        this.occupation = occupation;
+    }
+
+    // Method to display person information in the console
+    displayInfo() {
+        console.log("Name: " + this.name);
+        console.log("Age: " + this.age);
+        console.log("Occupation: " + this.occupation);
+    }
+
+    // Method to display person information in the HTML
+    displayInfoHTML() {
+        const personElement = document.getElementById('personOne');
+        personElement.innerHTML = `
+            <strong>Name:</strong> ${this.name}<br>
+            <strong>Age:</strong> ${this.age}<br>
+            <strong>Occupation:</strong> ${this.occupation}
+        `;
+    }
+}
+
+// Creating an object (instance) of the Person class
+const personOne = new Person('Jambert', 19, 'stupidyante');
+
+// Calling methods
+personOne.displayInfo();       
